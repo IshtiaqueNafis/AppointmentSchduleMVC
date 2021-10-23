0c034bccbc68b0f6f5d3a29fcd08ed0afd15150f
IshtiaqueNafis <nafu22@gmail.com>
2021-10-23 10:00:33 -0400

    added loginmodel and controller

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+Z
+*wwwroot/lib/bootstrap/dist/js/bootstrap.js,3\f\3ff342b19ca80328ad6fcc9e1ec7dbbee01b3662
---
+++ Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using AppointmentSchduleMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentSchduleMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
